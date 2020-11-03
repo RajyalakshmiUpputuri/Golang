09c2bd1ea86c31711a902debca6d6c6f21f56606
Rajyalakshmi Upputuri <35495227+RajyalakshmiUpputuri@users.noreply.github.com>
2020-11-03 23:08:03 +0530

    Add files via upload

---
+++ Gorilla/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type Article struct {
+	Title   string `json:"Title"`
+	Desc    string `json:"desc"`
+	Content string `json:"content"`
+}
+type Articles []Article
+
+func allArticles(w http.ResponseWriter, r *http.Request) {
+	articles := Articles{
+		Article{Title: "testing title", Desc: "Test Descrption", Content: "Hello world"},
+	}
+	fmt.Println("endpoint hit: all articles endpoint")
+	json.NewEncoder(w).Encode(articles)
+}
+func testPostArticles(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "the post  endpoint worked")
+}
+func homepage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "homepage endpoint worked")
+}
+func handleRequests() {
+	myRouter := mux.NewRouter().StrictSlash(true)
+	myRouter.HandleFunc("/", homepage)
+	myRouter.HandleFunc("/articles", allArticles).Methods("GET")
+	myRouter.HandleFunc("/articles", testPostArticles).Methods("POST")
+	log.Fatal(http.ListenAndServe(":8085", myRouter))
+
+}
+func main() {
+	handleRequests()
+}
